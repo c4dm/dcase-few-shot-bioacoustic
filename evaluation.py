6ad57aeb3557d114ab5e2d1d338a985aa0493591
inesnolas <madzimia@gmail.com>
2021-02-08 20:05:28 +0000

    several modifications: *used harmonic mean instead *if metadata with classes names not provided compute scores per audiofile, set, and Overall only. if this info is given, then scores per classes are also computed which takes into account the countings for all the audiofiles belonging to each class. *everythng is wrapped in a function now. *script requires GT files to be in the same folder structure as drive folder.

--- evaluation.py
+++ evaluation.py
@@ -7,7 +7,8 @@ import numpy as np
 import csv
 import metrics
 from datetime import datetime
-
+import copy
+from scipy import stats
 
 
 # v.0 use one single prediction file with all audios from eval sets together.
@@ -81,9 +82,9 @@ def compute_TP_FP_FN(pred_events_df, ref_events_df):
 def compute_scores_per_class_and_average_scores_per_set(counts_per_class):
 
     scores_per_class = {}
-    cumulative_fmeasure = 0
-    cumulative_precision = 0
-    cumulative_recall = 0
+    cumulative_fmeasure = []
+    cumulative_precision = []
+    cumulative_recall = []
     for cl in counts_per_class.keys():
         TP = counts_per_class[cl]["TP"]
         FP = counts_per_class[cl]["FP"]
@@ -97,16 +98,32 @@ def compute_scores_per_class_and_average_scores_per_set(counts_per_class):
 
         scores_per_class[cl] = {"precision": precision, "recall": recall, "f-measure":fmeasure }
 
-        cumulative_fmeasure = cumulative_fmeasure +fmeasure 
-        cumulative_precision = cumulative_precision+ precision
-        cumulative_recall = cumulative_recall + recall
+        cumulative_fmeasure.append(fmeasure)
+        cumulative_precision.append(precision)
+        cumulative_recall.append(recall)
     
     n_classes = len(list(counts_per_class.keys())) 
     # average scores in this set:
-    av_scores_set = {"av_precision": cumulative_precision/n_classes, "av_recall": cumulative_recall/n_classes, "av_fmeasure": cumulative_fmeasure/n_classes }
+    av_scores_set = {"precision": stats.hmean(cumulative_precision), "recall": stats.hmean(cumulative_recall), "f-measure": stats.hmean(cumulative_fmeasure) }
     return scores_per_class, av_scores_set
     
-def build_report(main_set_scores, scores_per_miniset, scores_per_class, save_path, main_set_name="EVAL", team_name="test_team" ):
+def compute_scores_from_counts(counts):
+    TP = counts["TP"]
+    FP = counts["FP"]
+    FN = counts["FN"]
+
+    precision = TP/(TP+FP) if TP+FP != 0 else 0
+    # precision = TP/(TP+FP) 
+    recall = TP/(FN+TP)
+    fmeasure = TP/(TP+0.5*(FP+FN))
+
+    scores = {"precision": precision, "recall": recall, "f-measure":fmeasure }
+    
+    return scores
+
+
+def build_report(main_set_scores, scores_per_miniset, scores_per_audiofile, save_path, main_set_name="EVAL", team_name="test_team" , **kwargs):
+    
 
     # datetime object containing current date and time
     now = datetime.now()
@@ -120,26 +137,42 @@ def build_report(main_set_scores, scores_per_miniset, scores_per_class, save_pat
             "report_date": date_string,
             "overall_scores": main_set_scores,
             "scores_per_subset": scores_per_miniset,
-            "scores_per_class": scores_per_class
+            "scores_per_audiofile": scores_per_audiofile
     }
+    if kwargs['scores_per_class']:
+        report["scores_per_class"] =  kwargs['scores_per_class']
 
     with open(os.path.join(save_path,"Evaluation_report_"+team_name+"_"+main_set_name+'_'+date_string+'.json'), 'w') as outfile:
         json.dump(report, outfile)
-    #     report_writer = csv.writer(outfile, delimeter = ',')
-    #     report_writer.writerow(["Team name: "+ team_name])
-    #     report_writer.writerow(["Set name: "+ main_set_name ])
-    #     report_writer.writerow([])
+
     return
+
+
+
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-pred_file_path', type=str, help='path to folder with prediction csv')
-    parser.add_argument('-ref_file_path', type=str, help='path to the reference events csvs')
+    parser.add_argument('-ref_file_path', type=str, help='path to the reference events csvs folder')
     parser.add_argument('-metadata', type=str, help="path for metadata json (map between audiofiles and classes")
     # parser.add_argument('-team_name', type=str, help='team identification') # make this optional?
     parser.add_argument('-dataset', type=str, help="which set to evaluate: EVAL, VAL, TRAIN")
+    parser.add_argument('-savepath', type=str, help="path where to save the report to")
     args = parser.parse_args()
     print(args)
+
+
+    #read Gt file structure: get subsets and paths for ref csvs make an inverted dictionary with audiofilenames as keys and folder as value
+    gt_file_structure = {}
+    gt_file_structure[args.dataset] = {}
+    inv_gt_file_structure = {}
+    list_of_subsets = os.listdir(args.ref_file_path)
+    for subset in list_of_subsets:
+        gt_file_structure[args.dataset][subset] = os.listdir(os.path.join(args.ref_file_path,subset))
+
+        for audiofile in gt_file_structure[args.dataset][subset]:
+            inv_gt_file_structure[audiofile] = subset
+
 
     #read prediction csv
     pred_csv = pd.read_csv(args.pred_file_path, dtype=str)
@@ -151,78 +184,120 @@ if __name__ == "__main__":
     for audiofilename in list(pred_events_by_audiofile.keys()):
         print(audiofilename)
         
+        # find wich subset the audiofile belongs to:
+        
         # for each audiofile list, load correcponding GT File (audiofilename.csv)
-        ref_events_this_audiofile = pd.read_csv(os.path.join(args.ref_file_path, audiofilename[0:-4]+'.csv'), dtype=str)
+        ref_events_this_audiofile = pd.read_csv(os.path.join(args.ref_file_path, inv_gt_file_structure[audiofilename], audiofilename[0:-4]+'.csv'), dtype=str)
         # compare and get counts: TP, FP .. 
         TP, FP, FN , total_n_events_in_audiofile= compute_TP_FP_FN(pred_events_by_audiofile[audiofilename], ref_events_this_audiofile )
 
         counts_per_audiofile[audiofilename]={"TP": TP, "FP": FP, "FN": FN, "total_n_pos_events": total_n_events_in_audiofile}
    
-    # using the key for classes => audiofiles,  # load sets metadata:
-    with open(args.metadata) as metadatafile:
-            dataset_metadata = json.load(metadatafile)
 
-
+    # this is optional:
+    if args.metadata:
+        # using the key for classes => audiofiles,  # load sets metadata:
+        with open(args.metadata) as metadatafile:
+                dataset_metadata = json.load(metadatafile)
+    else:
+        dataset_metadata = copy.deepcopy(gt_file_structure)
 
     # include audiofiles for which there were no predictions:
     list_all_audiofiles = []
     for miniset in dataset_metadata[args.dataset].keys():
-        for cl in dataset_metadata[args.dataset][miniset].keys():
-             list_all_audiofiles.extend(dataset_metadata[args.dataset][miniset][cl] )
+        if args.metadata:
+            for cl in dataset_metadata[args.dataset][miniset].keys():
+                list_all_audiofiles.extend(dataset_metadata[args.dataset][miniset][cl] )
+        else:
+            list_all_audiofiles.extend(dataset_metadata[args.dataset][miniset])
 
     for audiofilename in list_all_audiofiles:
         if audiofilename+".wav" not in counts_per_audiofile.keys():
-            ref_events_this_audiofile = pd.read_csv(os.path.join(args.ref_file_path, audiofilename+'.csv'), dtype=str)
+            ref_events_this_audiofile = pd.read_csv(os.path.join(args.ref_file_path, inv_gt_file_structure[audiofilename], audiofilename+'.csv'), dtype=str)
             total_n_pos_events_in_audiofile =  len(select_events_with_value(ref_events_this_audiofile, value = 'POS'))
             counts_per_audiofile[audiofilename+".wav"] = {"TP": 0, "FP": 0, "FN": total_n_pos_events_in_audiofile, "total_n_pos_events": total_n_pos_events_in_audiofile}
     
 
 
-    # aggregate the counts per class
+        
+    # aggregate the counts per class: 
     list_sets_in_mainset = list(dataset_metadata[args.dataset].keys())
     # list_classes_in_mainset = []
     # counts_per_class = {}
     counts_per_class_per_set = {}
     scores_per_class_per_set={}
-    av_scores_per_set={}
+    counts_per_set = {}
+    scores_per_set = {}
+    scores_per_audiofile={}
     for data_set in list_sets_in_mainset:
         print(data_set)
-        # list_classes_in_mainset.extend(list(dataset_metadata[args.dataset][data_set].keys()))        
-        list_classes_in_set = list(dataset_metadata[args.dataset][data_set].keys())
-   
-        counts_per_class_per_set[data_set] = {}
-    
-        for cl in list_classes_in_set:
-            print(cl)
-            list_audiofiles_this_class = dataset_metadata[args.dataset][data_set][cl]
-            tp = 0
-            fn = 0
-            fp = 0
-            total_n_pos_events_this_class = 0
-            for audiofile in list_audiofiles_this_class:
+
+
+        if args.metadata:
+            # list_classes_in_mainset.extend(list(dataset_metadata[args.dataset][data_set].keys()))        
+            list_classes_in_set = list(dataset_metadata[args.dataset][data_set].keys())
+
+            counts_per_class_per_set[data_set] = {}
+        
+            for cl in list_classes_in_set:
+                print(cl)
+                list_audiofiles_this_class = dataset_metadata[args.dataset][data_set][cl]
+                tp = 0
+                fn = 0
+                fp = 0
+                total_n_pos_events_this_class = 0
+                for audiofile in list_audiofiles_this_class:
+                    scores_per_audiofile[audiofile] = compute_scores_from_counts(counts_per_audiofile)
+
                     tp = tp + counts_per_audiofile[audiofile+".wav"]["TP"]
                     fn = fn + counts_per_audiofile[audiofile+".wav"]["FN"]
                     fp = fp + counts_per_audiofile[audiofile+".wav"]["FP"]
                     total_n_pos_events_this_class = total_n_pos_events_this_class + counts_per_audiofile[audiofile+".wav"]["total_n_pos_events"]
                 
-            # counts_per_class[cl] = {"TP":tp, "FN": fn, "FP": fp, "total_n_pos_events_this_class": total_n_pos_events_this_class}
-            counts_per_class_per_set[data_set][cl] = {"TP":tp, "FN": fn, "FP": fp, "total_n_pos_events_this_class": total_n_pos_events_this_class}
+                # counts_per_class[cl] = {"TP":tp, "FN": fn, "FP": fp, "total_n_pos_events_this_class": total_n_pos_events_this_class}
+                counts_per_class_per_set[data_set][cl] = {"TP":tp, "FN": fn, "FP": fp, "total_n_pos_events_this_class": total_n_pos_events_this_class}
 
-        #  compute scores per class. # aggregate scores per sets: will this be an average of scores across classes of each set?
-        scores_per_class_per_set[data_set], av_scores_per_set[data_set] = compute_scores_per_class_and_average_scores_per_set(counts_per_class_per_set[data_set])  
+            #  compute scores per class. # aggregate scores per sets: will this be an average of scores across classes of each set?
+            scores_per_class_per_set[data_set], scores_per_set[data_set] = compute_scores_per_class_and_average_scores_per_set(counts_per_class_per_set[data_set])  
+        
+        
+        else:
+            list_audiofiles_in_set = dataset_metadata[args.dataset][data_set]
+            tp = 0
+            fn = 0
+            fp = 0
+            total_n_pos_events_this_set = 0
+            for audiofile in  list_audiofiles_in_set:
+
+                scores_per_audiofile[audiofile] = compute_scores_from_counts(counts_per_audiofile)
+                tp = tp + counts_per_audiofile[audiofile+".wav"]["TP"]
+                fn = fn + counts_per_audiofile[audiofile+".wav"]["FN"]
+                fp = fp + counts_per_audiofile[audiofile+".wav"]["FP"]
+                total_n_pos_events_this_set = total_n_pos_events_this_set + counts_per_audiofile[audiofile+".wav"]["total_n_pos_events"]
+                counts_per_set[data_set] = {"TP":tp, "FN": fn, "FP": fp, "total_n_pos_events_this_set": total_n_pos_events_this_class}
             
-    
+            #  compute scores per class. # aggregate scores per sets: will this be an average of scores across classes of each set?
+            scores_per_set[data_set]= compute_scores_from_counts(counts_per_set[data_set])
+                    
+
     #average scores per all sets in the eval set 
     # av(dc_scores, ME_scores, ML_scores)
-    Overall_scores = {"precision" : sum([av_scores_per_set[dt]["av_precision"] for dt in av_scores_per_set.keys()])/ len(list(av_scores_per_set.keys())) , 
-                    "recall": sum([av_scores_per_set[dt]["av_recall"] for dt in av_scores_per_set.keys()])/ len(list(av_scores_per_set.keys())) ,
-                    "fmeasure": sum([av_scores_per_set[dt]["av_fmeasure"] for dt in av_scores_per_set.keys()])/ len(list(av_scores_per_set.keys())) ,
+    
+    Overall_scores = {"precision" : stats.hmean([scores_per_set[dt]["precision"] for dt in scores_per_set.keys()]) , 
+                    "recall":  stats.hmean([scores_per_set[dt]["recall"] for dt in scores_per_set.keys()]) ,
+                    "fmeasure":  stats.hmean([scores_per_set[dt]["f-measure"] for dt in scores_per_set.keys()])
                     }
+   
     
-    
-    build_report(Overall_scores, av_scores_per_set, scores_per_class_per_set, 
+    if args.metadata:
+        build_report(Overall_scores, scores_per_set, scores_per_audiofile,
+                scores_per_class=scores_per_class_per_set,
                 save_path="/mnt/c/Users/madzi/Dropbox/QMUL/PHD/DCASE2021_few-shot_bioacoustics_challenge/dcase-few-shot-bioacoustic/", 
-                main_set_name=args.dataset )
+                main_set_name = args.dataset )
+    else:
+        build_report(Overall_scores, scores_per_set, scores_per_audiofile,
+                save_path="/mnt/c/Users/madzi/Dropbox/QMUL/PHD/DCASE2021_few-shot_bioacoustics_challenge/dcase-few-shot-bioacoustic/", 
+                main_set_name=args.dataset  )
 
 
 
