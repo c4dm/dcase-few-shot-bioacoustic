1d03c253f6c01649cc4232287bde3041b2b5642e
inesnolas <madzimia@gmail.com>
2021-02-17 11:55:21 +0000

    implemented harmonic mean by adding a very small values to the zero entries, cleaned code and added verification for wrong header in prediction file.

--- evaluation.py
+++ evaluation.py
@@ -72,15 +72,13 @@ def compute_TP_FP_FN(pred_events_df, ref_events_df):
 
     m_unk = metrics.match_events(ref_2nd_round, pred_2nd_round, min_iou = 0.5)
 
-    print("Positive matches between Ref and Pred :", m_pos)
-    print("matches with Unknown events: ", m_unk)
+    # print("# Positive matches between Ref and Pred :", len(m_pos))
+    # print("# matches with Unknown events: ", len(m_unk))
     
     TP = len(m_pos)
     FP = pred_1st_round.shape[1] - TP - len(m_unk)
     
     ## compute unmatched pos ref events:
-    # matched_events_ref = TP + len(m_unk)
-    # count_unmached_ref_events = len(ref_events_df)- matched_events_ref
     count_unmached_pos_ref_events = len(ref_pos_indexes) - TP
 
     FN = count_unmached_pos_ref_events
@@ -103,10 +101,13 @@ def compute_scores_per_class_and_average_scores_per_set(counts_per_class):
         FN = counts_per_class[cl]["FN"]
 
             
-        precision = TP/(TP+FP) if TP+FP != 0 else 0
+        # to compute the harmonic mean we need to have all entries as non zero
+        precision = TP/(TP+FP) if TP+FP != 0 else 0.00001  # case where no predictions were made 
+        if precision == 0:
+            precision = 0.00001 
         # precision = TP/(TP+FP) 
-        recall = TP/(FN+TP)
-        fmeasure = TP/(TP+0.5*(FP+FN))
+        recall = TP/(FN+TP) if TP != 0 else 0.00001
+        fmeasure = TP/(TP+0.5*(FP+FN)) if TP != 0 else 0.00001
 
         scores_per_class[cl] = {"precision": precision, "recall": recall, "f-measure":fmeasure }
 
@@ -114,15 +115,10 @@ def compute_scores_per_class_and_average_scores_per_set(counts_per_class):
         cumulative_precision.append(precision)
         cumulative_recall.append(recall)
     
-    n_classes = len(counts_per_class) 
+    # n_classes = len(counts_per_class) 
     # average scores in this set:
-    #harmonic mean with zero entries gives zeros :( )
-    #  av_scores_set = {"precision": stats.hmean(cumulative_precision), "recall": stats.hmean(cumulative_recall), "f-measure": stats.hmean(cumulative_fmeasure) }
-    av_scores_set = {"precision": sum(cumulative_precision)/n_classes, "recall": sum(cumulative_recall)/n_classes, "f-measure": sum(cumulative_fmeasure)/n_classes }
-    
-    # # instead of average per class average per audiofile for consistency when we do not have the metadata
-    # av_scores_set = {"precision": sum(cumulative_precision)/n_classes, "recall": sum(cumulative_recall)/n_classes, "f-measure": sum(cumulative_fmeasure)/n_classes }
-    
+    av_scores_set = {"precision": stats.hmean(cumulative_precision), "recall": stats.hmean(cumulative_recall), "f-measure": stats.hmean(cumulative_fmeasure) }
+       
     return scores_per_class, av_scores_set
     
 def compute_scores_from_counts(counts):
@@ -130,10 +126,12 @@ def compute_scores_from_counts(counts):
     FP = counts["FP"]
     FN = counts["FN"]
 
-    precision = TP/(TP+FP) if TP+FP != 0 else 0
-    # precision = TP/(TP+FP) 
-    recall = TP/(FN+TP)
-    fmeasure = TP/(TP+0.5*(FP+FN))
+    # to compute the harmonic mean we need to have all entries as non zero
+    precision = TP/(TP+FP) if TP+FP != 0 else 0.00001  # case where no predictions were made 
+    if precision == 0:
+        precision = 0.00001 
+    recall = TP/(FN+TP) if TP != 0 else 0.00001
+    fmeasure = TP/(TP+0.5*(FP+FN)) if TP != 0 else 0.00001
 
     scores = {"precision": precision, "recall": recall, "f-measure":fmeasure }
     
@@ -166,20 +164,14 @@ def build_report(main_set_scores, scores_per_miniset, scores_per_audiofile, save
     return
 
 def evaluate( pred_file_path, ref_file_path, team_name, dataset, savepath, metadata=[]):
-    #     parser.add_argument('-pred_file_path', type=str, help='path to folder with prediction csv')
-    #     parser.add_argument('-ref_file_path', type=str, help='path to the reference events csvs folder')
-    #     parser.add_argument('-metadata', type=str, help="path for metadata json (map between audiofiles and classes")
-    #     # parser.add_argument('-team_name', type=str, help='team identification') # make this optional?
-    #     parser.add_argument('-dataset', type=str, help="which set to evaluate: EVAL, VAL, TRAIN")
-    #     parser.add_argument('-savepath', type=str, help="path where to save the report to")
-        
+
+    print("\nevaluatiion for:", team_name, dataset)
     #read Gt file structure: get subsets and paths for ref csvs make an inverted dictionary with audiofilenames as keys and folder as value
     gt_file_structure = {}
     gt_file_structure[dataset] = {}
     inv_gt_file_structure = {}
     list_of_subsets = os.listdir(ref_file_path)
     for subset in list_of_subsets:
-        # gt_file_structure[dataset][subset] = os.listdir(os.path.join(ref_file_path,subset)) # change to read only .csvs, but save it as .wav?
         gt_file_structure[dataset][subset] = [os.path.basename(fl)[0:-4]+'.wav' for fl in glob.glob(os.path.join(ref_file_path,subset,"*.csv"))]
         for audiofile in gt_file_structure[dataset][subset]:
             inv_gt_file_structure[audiofile] = subset
@@ -187,28 +179,29 @@ def evaluate( pred_file_path, ref_file_path, team_name, dataset, savepath, metad
 
     #read prediction csv
     pred_csv = pd.read_csv(pred_file_path, dtype=str)
+    #verify headers:
+    if list(pred_csv.columns) ==  ["Audiofilename","Starttime","Endtime"]:
+        print('Please correct the header of the prediction file. This should be "Audiofilename","Starttime","Endtime"')
     #  parse prediction csv
-    # split file into lists of events for the same audiofile.
+    #  split file into lists of events for the same audiofile.
     pred_events_by_audiofile = dict(tuple(pred_csv.groupby('Audiofilename')))
 
     counts_per_audiofile = {}
     for audiofilename in list(pred_events_by_audiofile.keys()):
-        print(audiofilename)
-        
-        # find wich subset the audiofile belongs to:
-        
+       
+               
         # for each audiofile list, load correcponding GT File (audiofilename.csv)
         ref_events_this_audiofile_all = pd.read_csv(os.path.join(ref_file_path, inv_gt_file_structure[audiofilename], audiofilename[0:-4]+'.csv'), dtype={'Starttime':np.float64, 'Endtime': np.float64})
-        #Remove the 5 shots from GT here! 
-
+        
+        #Remove the 5 shots from GT:
         ref_events_this_audiofile = remove_shots_from_ref(ref_events_this_audiofile_all, number_shots=5)
+        
         # compare and get counts: TP, FP .. 
         TP, FP, FN , total_n_events_in_audiofile= compute_TP_FP_FN(pred_events_by_audiofile[audiofilename], ref_events_this_audiofile )
 
         counts_per_audiofile[audiofilename]={"TP": TP, "FP": FP, "FN": FN, "total_n_pos_events": total_n_events_in_audiofile}
-   
+        print(audiofilename, counts_per_audiofile[audiofilename])
 
-    # this is optional:
     if metadata:
         # using the key for classes => audiofiles,  # load sets metadata:
         with open(metadata) as metadatafile:
@@ -234,22 +227,18 @@ def evaluate( pred_file_path, ref_file_path, team_name, dataset, savepath, metad
 
 
         
-    # aggregate the counts per class: 
+    # aggregate the counts per class or subset: 
     list_sets_in_mainset = list(dataset_metadata[dataset].keys())
-    # list_classes_in_mainset = []
-    # counts_per_class = {}
+
     counts_per_class_per_set = {}
     scores_per_class_per_set={}
     counts_per_set = {}
     scores_per_set = {}
     scores_per_audiofile={}
     for data_set in list_sets_in_mainset:
-        print(data_set)
+        # print(data_set)
         
-
         if metadata:
-
-            # list_classes_in_mainset.extend(list(dataset_metadata[args.dataset][data_set].keys()))        
             list_classes_in_set = list(dataset_metadata[dataset][data_set].keys())
 
             counts_per_class_per_set[data_set] = {}
@@ -258,7 +247,7 @@ def evaluate( pred_file_path, ref_file_path, team_name, dataset, savepath, metad
             fp_set = 0
             total_n_events_set = 0
             for cl in list_classes_in_set:
-                print(cl)
+                # print(cl)
                 list_audiofiles_this_class = dataset_metadata[dataset][data_set][cl]
                 tp = 0
                 fn = 0
@@ -280,9 +269,9 @@ def evaluate( pred_file_path, ref_file_path, team_name, dataset, savepath, metad
                 counts_per_class_per_set[data_set][cl] = {"TP":tp, "FN": fn, "FP": fp, "total_n_pos_events_this_class": total_n_pos_events_this_class}
                 counts_per_set[data_set] = {"TP":tp_set, "FN": fn_set, "FP": fp_set, "total_n_pos_events_this_set": total_n_events_set}
             
-            #  compute scores per class. # aggregate scores per sets: will this be an average of scores across classes of each set?
+            #  compute scores per subset.  
             scores_per_set[data_set]= compute_scores_from_counts(counts_per_set[data_set])
-            #  compute scores per class. # aggregate scores per sets: will this be an average of scores across classes of each set?
+            #  compute scores per class
             scores_per_class_per_set[data_set], _ = compute_scores_per_class_and_average_scores_per_set(counts_per_class_per_set[data_set])  
             
         
@@ -301,26 +290,21 @@ def evaluate( pred_file_path, ref_file_path, team_name, dataset, savepath, metad
                 total_n_pos_events_this_set = total_n_pos_events_this_set + counts_per_audiofile[audiofile]["total_n_pos_events"]
                 counts_per_set[data_set] = {"TP":tp, "FN": fn, "FP": fp, "total_n_pos_events_this_set": total_n_pos_events_this_set}
             
-            #  compute scores per class. # aggregate scores per sets: will this be an average of scores across classes of each set?
+            #  compute scores per subset
             scores_per_set[data_set]= compute_scores_from_counts(counts_per_set[data_set])
                     
-
-    #average scores per all sets in the eval set 
-    # av(dc_scores, ME_scores, ML_scores)
-    
-    # if the scores for a subset are 0, harmonic mean gives zero( div by zero should give error!)
-    # Overall_scores = {"precision" : stats.hmean([scores_per_set[dt]["precision"] for dt in scores_per_set.keys()]) , 
-    #                 "recall":  stats.hmean([scores_per_set[dt]["recall"] for dt in scores_per_set.keys()]) ,
-    #                 "fmeasure":  stats.hmean([scores_per_set[dt]["f-measure"] for dt in scores_per_set.keys()])
-    #                 }
-    Overall_scores = {"precision" : sum([scores_per_set[dt]["precision"] for dt in scores_per_set.keys()])/len(scores_per_set) , 
-                    "recall":  sum([scores_per_set[dt]["recall"] for dt in scores_per_set.keys()])/len(scores_per_set) ,
-                    "fmeasure":  sum([scores_per_set[dt]["f-measure"] for dt in scores_per_set.keys()])/len(scores_per_set)
+    Overall_scores = {"precision" : stats.hmean([scores_per_set[dt]["precision"] for dt in scores_per_set.keys()]), 
+                    "recall":  stats.hmean([scores_per_set[dt]["recall"] for dt in scores_per_set.keys()]) ,
+                    "fmeasure":  stats.hmean([scores_per_set[dt]["f-measure"] for dt in scores_per_set.keys()])
                     }
-   
     
+    # Overall_scores = {"precision" : sum([scores_per_set[dt]["precision"] for dt in scores_per_set.keys()])/len(scores_per_set) , 
+    #                 "recall":  sum([scores_per_set[dt]["recall"] for dt in scores_per_set.keys()])/len(scores_per_set) ,
+    #                 "fmeasure":  sum([scores_per_set[dt]["f-measure"] for dt in scores_per_set.keys()])/len(scores_per_set)
+    #                 }
+    print("\nOverall_scores:",  Overall_scores)
+    print("\nwriting report")
     if metadata:
-           
         build_report(Overall_scores, scores_per_set, scores_per_audiofile,
                 savepath, 
                 dataset,
@@ -334,6 +318,7 @@ def evaluate( pred_file_path, ref_file_path, team_name, dataset, savepath, metad
     
     return
 
+
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
@@ -344,7 +329,7 @@ if __name__ == "__main__":
     parser.add_argument('-dataset', type=str, help="which set to evaluate: EVAL, VAL, TRAIN")
     parser.add_argument('-savepath', type=str, help="path where to save the report to")
     args = parser.parse_args()
-    print(args)
+    # print(args)
 
     # evaluate( args.pred_file_path, args.ref_file_path, args.team_name, args.dataset, args.savepath, args.metadata)
     # evaluate( args.pred_file_path, args.ref_file_path, args.team_name, args.dataset, args.savepath)
@@ -402,14 +387,4 @@ if __name__ == "__main__":
     evaluate( pred_file_path, ref_file_path, team_name, dataset, savepath, metadata)
     evaluate( pred_file_path, ref_file_path, team_name, dataset, savepath)
 
-
-
-# TODO make verifications for cases where audiofile does not have a corresponding csv file  ex: line 189
-# TODO make sure headers are the correct ones, without spaces!
-# TODO add to report prediction file used. maybe the path.
-# TODO add to report some space for notes like model used or variant of something
-# TODO save matches ?
-# TODO remove from GT the 5 shots 
-# TODO change the match events min IOU threshold for 50%
-# TODO harmonic mean computation
 
